73447702e6d9a66346ab09b6a68e28294d7fad29
harshmighlani <harshmighlani@gmail.com>
2019-07-12 09:27:48 +0530

    initial commit

---
+++ SampleHelloWorld/SampleHelloWorld/src/main/java/com/test/SampleHelloWorld/SampleHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.test.SampleHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleHelloWorldApplication.class, args);
+	}
+
+}
